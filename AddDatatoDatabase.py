0876066ffc48f4bd4af28316bdd3718fcdbff4e3
Ashish Satish Bhosale <142018023+AshishSatish@users.noreply.github.com>
2025-07-31 13:32:15 +0530

    Update AddDatatoDatabase.py
    Data model was changed from just storing the last attendance to storing the entire log

--- AddDatatoDatabase.py
+++ AddDatatoDatabase.py
@@ -4,7 +4,7 @@ from firebase_admin import db
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': ""  
+    'databaseURL': "https://automated-attendance-691c3-default-rtdb.firebaseio.com"  
 })
 
 ref = db.reference('Students')
@@ -13,33 +13,92 @@ data = {
     "122B1F011":
         {
             "name": "Ashish Bhosale",
-            "major": "Information Tech",
+            "major": "Information Technology",
             "starting_year": 2022,
             "total_attendance": 0,
             "Division": "A",
             "year": 4,
-            "last_attendance_time": "2022-12-11 00:54:34"
+            "last_attendance_time": "2025-07-18 00:54:34",
+            "attendance_log": []
         },
-    "852741":
+    "122B1F041":
         {
-            "name": "Emly Blunt",
-            "major": "Economics",
-            "starting_year": 2021,
+            "name": "Shreyas Jawale",
+            "major": "Information Technology",
+            "starting_year": 2022,
             "total_attendance": 0,
             "Division": "A",
-            "year": 1,
-            "last_attendance_time": "2022-12-11 00:54:34"
+            "year": 4,
+            "last_attendance_time": "2025-07-18 00:54:34",
+            "attendance_log": []
         },
-    "963852":
+    "122B1F023":
         {
-            "name": "Elon Musk",
-            "major": "Physics",
-            "starting_year": 2020,
+            "name": "Ritesh Dudhade",
+            "major": "Information Technology",
+            "starting_year": 2022,
             "total_attendance": 0,
-            "Division": "G",
-            "year": 2,
-            "last_attendance_time": "2022-12-11 00:54:34"
+            "Division": "B",
+            "year": 4,
+            "last_attendance_time": "2025-07-18 00:54:34",
+            "attendance_log": []
+        },
+    "122B1F005":
+        {
+            "name": "Sankalp Banginwar",
+            "major": "Computer Science",
+            "starting_year": 2022,
+            "total_attendance": 0,
+            "Division": "A",
+            "year": 4,
+            "last_attendance_time": "2025-07-18 00:54:34",
+            "attendance_log": []
+        },
+    "122B1F004":
+        {
+            "name": "Sushil Bang",
+            "major": "Computer Science",
+            "starting_year": 2022,
+            "total_attendance": 0,
+            "Division": "B",
+            "year": 4,
+            "last_attendance_time": "2025-07-18 00:54:34",
+            "attendance_log": []
+        },
+    "122B1F002":
+        {
+            "name": "Abhinandan Ashtekar",
+            "major": "Information Technology",
+            "starting_year": 2022,
+            "total_attendance": 0,
+            "Division": "B",
+            "year": 4,
+            "last_attendance_time": "2025-07-18 00:54:34",
+            "attendance_log": []
+        },
+    "123B1B024":
+        {
+            "name": "Atharv Rao",
+            "major": "Computer Science",
+            "starting_year": 2023,
+            "total_attendance": 0,
+            "Division": "A",
+            "year": 3,
+            "last_attendance_time": "2025-07-18 00:54:34",
+            "attendance_log": []
+        },
+    "122B1F016":
+        {
+            "name": "Harshal Chavan",
+            "major": "Information Technology",
+            "starting_year": 2022,
+            "total_attendance": 0,
+            "Division": "A",
+            "year": 4,
+            "last_attendance_time": "2025-07-18 00:54:34",
+            "attendance_log": []
         }
+    
 }
 
 for key, value in data.items():
